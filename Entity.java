4945d648706d2e9969d6ccc71a6b626277fee604
Andrew-habashy <60792298+Andrew-habashy@users.noreply.github.com>
2020-07-13 13:27:56 -0400

    Create Entity.java

---
+++ Entity.java
@@ -0,0 +1,54 @@
+package eecs.project;
+
+
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class Entity {
+	private int x;
+	private int y;
+	private int Size;
+	
+	public Entity(int Size){
+		this.Size = Size;
+	}
+	
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public void setPostion(int x, int y)
+	{
+		this.x= x;
+		this.y =y;
+	}
+	
+	public void move(int dx, int dy){
+		x +=dx;
+		y +=dy;
+	}
+	public Rectangle getBound(){
+		return new Rectangle (x,y,Size, Size);
+	}
+	
+	public boolean isCollsion(Entity e){
+		if(e == this)return false;
+		return getBound().intersects(e.getBound());
+	}
+	public void render (Graphics2D g2d){
+		g2d.fillRect(x +1, y +1, Size-2, Size-2);
+	}
+	
+	
+
+}
